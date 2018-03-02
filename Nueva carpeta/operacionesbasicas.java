cbadb29ca5bfff2c960db8cf982ded4a553156d2
juancaromero <36993586+juancaromero@users.noreply.github.com>
2018-03-02 10:11:33 -0500

    Add files via upload

---
+++ Nueva carpeta/operacionesbasicas.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios;
+
+/**
+ *
+ * @author Jhon Pineda
+ */
+public class Ejercicios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero=0,numero1=245,numero2=123;
+        numero=numero1+numero2;
+        System.out.println(numero);
+        
+        int primernumero=0,segundonumero=657,tercernumero=480;
+        primernumero=segundonumero-tercernumero;
+        
+        System.out.println(primernumero);
+        int valor1=0,valor2=12,valor3=4;
+        valor1=valor2*valor3;
+        System.out.println(valor1);
+        
+        int x=0,y=12000,z=300;
+        x=y/z;
+        System.out.println(x);
+        
+        double t=0,w=748;
+        t=0.05*w;
+        System.out.println(t);
+        
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
